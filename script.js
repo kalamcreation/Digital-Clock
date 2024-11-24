f9fa0ef0ad62534956f37b80b052d2d4ee12fe6c
kalamcreation <mdabulkalamosman@gmail.com>
2024-11-24 22:05:13 +0600

    project done

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let amPm = document.getElementById("amPm"); // Add an element for AM/PM
 
-
-
-setInterval(()=>{
+setInterval(() => {
     let currentTime = new Date();
 
-    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();;
-    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();;
+    let hours = currentTime.getHours();
+    let period = hours >= 12 ? "PM" : "AM"; // Determine AM/PM
 
-},1000)
+    // Convert hours to 12-hour format
+    hours = hours % 12 || 12; // If hours is 0, make it 12
+
+    hrs.innerHTML = (hours < 10 ? "0" : "") + hours;
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+    amPm.innerHTML = period; // Set AM/PM
+}, 1000);
